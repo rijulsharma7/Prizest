2f786a62c5b0d97a6d2b5bab9456b1af9bd21537
Parv Gupta <parvg555@gmail.com>
2021-06-10 15:59:46 +0530

    login page complete

---
+++ prizest/login/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import NewUserForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import authenticate
+from django.contrib import messages
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html',{})
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect('dashboard')
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request,'login.html',context={"login_form":form})
+
+def signup(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect('dashboard')
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm
+    return render(request,'signup.html',{"register_form":form})
+
+def logout(request):
+    auth_logout(request)
+    return redirect('index')
